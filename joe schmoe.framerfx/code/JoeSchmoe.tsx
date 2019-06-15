bf9d4d8b63200f0e413b0a2e929537c7bccd18f7
Elliot Nolten <elliot@nolten.co>
2019-06-15 21:46:02 +0200

    rewritten with new Framer API

--- joe schmoe.framerfx/code/JoeSchmoe.tsx
+++ joe schmoe.framerfx/code/JoeSchmoe.tsx
@@ -1,83 +1,49 @@
 import * as React from "react"
-import { PropertyControls, ControlType } from "framer"
+import { Frame, addPropertyControls, ControlType } from "framer"
 
-
-type Props = {
-  name: string
-  gender: string
-  width: number
-  height: number
-  radius: number
-  border: string
-  borderWidth: number
-  background: string
-}
-
-type State = {
-  gender: string
-  genderFromProps: string
-}
-
-export class JoeSchmoe extends React.Component<Partial<Props>, State> {
-  render() {
-    const { name, radius, border, borderWidth, background } = this.props
-    const { gender } = this.state
-    const namePart = name.length > 0 ? name : "random"
-    const generatedURL = `https://joeschmoe.io/api/v1/${gender}/${namePart}/`
+export function JoeSchmoe(props) {
+    const generatedURL = `https://joeschmoe.io/api/v1/${props.gender}/${
+        props.name
+    }/`
     return (
-      <div
-        style={{
-          width:"100%",
-          height: "100%",
-          background: `${background} url(${generatedURL}) no-repeat center center`,
-          borderRadius: `${radius}px`,
-          border: `${borderWidth}px solid ${border}`,
-        }}
-      />
+        <Frame
+            image={generatedURL}
+            backgroundColor={props.background}
+            border={`${props.borderWidth}px solid ${props.border}`}
+            borderRadius={`${props.radius}%`}
+            width={props.width}
+            height={props.height}
+        />
     )
-  }
+}
 
-  state = {
-    gender: this.props.gender,
-    genderFromProps: this.props.gender,
-  }
-
-  // Allow setting the gender from within the property panel.
-  static getDerivedStateFromProps(props: Props, state: State) {
-    if (props.gender !== state.genderFromProps) {
-      return { gender: props.gender, genderFromProps: props.gender };
-    }
-  }
-
-  static defaultProps: Props = {
-    name: "Pamela",
-    gender: "female",
-    width: 200,
-    height: 200,
+JoeSchmoe.defaultProps = {
+    width: 100,
+    height: 100,
+    name: "Joe",
+    gender: "male",
     radius: 100,
     border: "rgba(0,0,0,1)",
     borderWidth: 1,
-    background: "rgba(255,255,255,1)"
-  }
+    background: "rgba(255,255,255,1)",
+}
 
-  static propertyControls: PropertyControls<Props> = {
+addPropertyControls(JoeSchmoe, {
     gender: {
-      type: ControlType.SegmentedEnum,
-      options: ["female","male"],
-      optionTitles: ["🙋🏻‍♀️ Female","🙋🏻‍♂️ Male"],
-      title: "Gender"
+        type: ControlType.SegmentedEnum,
+        options: ["female", "male"],
+        optionTitles: ["🙋🏻‍♀️ Female", "🙋🏻‍♂️ Male"],
+        title: "Gender",
     },
     name: { type: ControlType.String, title: "Name" },
-    radius: { type: ControlType.Number, min: 0, title: "Radius"},
+    radius: { type: ControlType.Number, min: 0, title: "Radius" },
     border: { type: ControlType.Color, title: "Border" },
     borderWidth: {
-      type: ControlType.Number,
-      title: " ",
-      min: 1,
-      max: 5,
-      displayStepper: true
+        type: ControlType.Number,
+        title: "Border Width",
+        min: 0,
+        max: 50,
+        displayStepper: true,
     },
-    background: { type: ControlType.Color, title: "Background"},
-  }
-
-}
+    background: { type: ControlType.Color, title: "Background" },
+})
